27100dfb706b695062fb18a91d96f2c1b086dcdd
Rong_on_SelfPc <rongjianqi@stu.cqu.edu.cn>
2025-12-17 10:58:22 +0800

    修改了side的流量

--- src/Solver_openmp_ad_No_preci.cpp
+++ src/Solver_openmp_ad_No_preci.cpp
@@ -1625,7 +1625,7 @@ reverse_mode<double> PNMsolver::func_append_kong2(reverse_mode<double>& Pi, reve
     } else {
     }
   }
-  return RETURN - 0.0003 * 0.01 * 0.01 * 0.01 / 60;
+  return RETURN - 0.00015 * 0.01 * 0.01 * 0.01 / 60;
 };
 
 reverse_mode<double> PNMsolver::func_BULK_PHASE_FLOW_kong(reverse_mode<double>& Pi, reverse_mode<double>* Pjs, reverse_mode<double>& Wi, reverse_mode<double>* Wjs, int Pore_id) {
